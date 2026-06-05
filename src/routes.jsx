763ed478c4645faf2b117b543cb22f7f26f62aa2
cH0NKIIs34L <chonkyseal.work@gmail.com>
2026-06-05 09:52:57 +0800

    chore(config): add router page shell

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,29 @@
 import { createBrowserRouter } from 'react-router';
 import App from './pages/App/App';
+import CartPage from './pages/CartPage/CartPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
+import HomePage from './pages/HomePage/HomePage';
+import ShopPage from './pages/ShopPage/ShopPage';
 
 const routes = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'shop',
+        element: <ShopPage />,
+      },
+      {
+        path: 'cart',
+        element: <CartPage />,
+      },
+    ],
   },
 ]);
 
